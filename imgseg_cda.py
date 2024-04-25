694b879c43b7bcd94e532cd54af994c64907d32a
c-bruce <callum.bruce1@gmail.com>
2024-04-25 21:20:39 +0100

    Implemented callable application and saving of segmented image to file

--- imgseg_cda.py
+++ imgseg_cda.py
@@ -1,1 +1,25 @@
-# Run imgseg_cda
+import argparse
+import cv2
+import supervision as sv
+
+from src.segmentors import SamSegmentor
+
+
+def main(input_file):
+    image = cv2.imread(input_file)
+
+    segmentor = SamSegmentor()
+    results = segmentor.mask_generator.generate(image)
+    detections = sv.Detections.from_sam(sam_result=results)
+    annotated_image = sv.MaskAnnotator().annotate(scene=image.copy(), detections=detections)
+
+    cv2.imwrite(f"{input_file.split('.')[0]}_segmented_output.png", annotated_image)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('input_file', type=str, help='Path to input image')
+
+    args = parser.parse_args()
+
+    main(args.input_file)
--- src/segmentors.py
+++ src/segmentors.py
@@ -1,9 +1,9 @@
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
 
-from constants import DEVICE
+from src.constants import DEVICE
 
 class SamSegmentor:
-    def __init__(self, checkpoint_path='sam_vit_h_4b8939.pth', model_type='vit_h'):
+    def __init__(self, checkpoint_path='src/sam_vit_h_4b8939.pth', model_type='vit_h'):
         self.checkpoint_path = checkpoint_path
         self.model_type = model_type
         self.model = sam_model_registry[self.model_type](checkpoint=self.checkpoint_path).to(device=DEVICE)
